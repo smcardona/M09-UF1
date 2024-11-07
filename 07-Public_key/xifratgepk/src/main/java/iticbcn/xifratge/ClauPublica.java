6936d584dd4a5f2426a2b2b4bdd3a081c1e381ee
Santiago Muñoz <2023_santiago.munoz@iticbcn.cat>
2024-11-07 18:13:51 +0100

    Added implementation of RSA in exercise 07

--- 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/ClauPublica.java
+++ 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/ClauPublica.java
@@ -3,21 +3,37 @@
 package iticbcn.xifratge;
 
 import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
+import javax.crypto.Cipher;
+
 class ClauPublica {
   
+  private static final String ALGORISME_XIFRATGE = "RSA";
+  private static final int AMPLADA_CLAU = 2048;
+  
   public KeyPair generaParellClausRSA() throws Exception {
-    return null;
+    KeyPairGenerator generador = KeyPairGenerator.getInstance(ALGORISME_XIFRATGE);
+    generador.initialize(AMPLADA_CLAU);
+    return generador.generateKeyPair();
   }
 
   public byte[] xifraRSA(String msg, PublicKey clauPublica) throws Exception {
-    return null;
+    Cipher xifrador = Cipher.getInstance(ALGORISME_XIFRATGE);
+    xifrador.init(Cipher.ENCRYPT_MODE, clauPublica);
+    
+    byte[] missatgeXifrat = xifrador.doFinal(msg.getBytes());
+    return missatgeXifrat;
   }
 
-  public String desxifraRSA(byte[] msgXifrat, PrivateKey ClauPrivada) {
-    return null;
+  public String desxifraRSA(byte[] msgXifrat, PrivateKey clauPrivada) throws Exception {
+    Cipher xifrador = Cipher.getInstance(ALGORISME_XIFRATGE);
+    xifrador.init(Cipher.DECRYPT_MODE, clauPrivada);
+
+    byte[] missatgeDesxifrat = xifrador.doFinal(msgXifrat);
+    return new String(missatgeDesxifrat);
   }
   
 }
