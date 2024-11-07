a6f786a9d196166be6be323bac2044d9924a9eee
Santiago Muñoz <2023_santiago.munoz@iticbcn.cat>
2024-11-07 17:10:57 +0100

    Pushing base folders for Exercise 07

---
+++ 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/ClauPublica.java
@@ -0,0 +1,23 @@
+
+
+package iticbcn.xifratge;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+class ClauPublica {
+  
+  public KeyPair generaParellClausRSA() throws Exception {
+    return null;
+  }
+
+  public byte[] xifraRSA(String msg, PublicKey clauPublica) throws Exception {
+    return null;
+  }
+
+  public String desxifraRSA(byte[] msgXifrat, PrivateKey ClauPrivada) {
+    return null;
+  }
+  
+}
